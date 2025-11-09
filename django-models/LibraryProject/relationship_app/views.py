783c38497e2fee94a487c0f066c75b12af9a11a6
Patrick-startech <asamoahpatrick1999@gmail.com>
2025-11-09 14:37:44 -0500

    Add files via upload

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -18,16 +18,17 @@ class LibraryDetailView(DetailView):
 
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib.auth.forms import UserCreationForm
 
-def login_view(request):
+def register(request):
     if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
-            user = form.get_user()
+            user = form.save()
             login(request, user)
-            return redirect('list_books')  # Redirect to any page after login
+            return redirect('list_books')
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
