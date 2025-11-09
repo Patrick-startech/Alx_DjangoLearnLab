e8bcd70a6143e5984d089ed782857d8cf1da0917
Patrick-startech <asamoahpatrick1999@gmail.com>
2025-11-09 13:24:03 -0500

    Add files via upload

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,11 +8,10 @@ def list_books(request):
     books = Book.objects.all()  # Required for milestone check
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-
 from django.views.generic.detail import DetailView
 from .models import Library
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'  # Required for milestone check
     context_object_name = 'library'
