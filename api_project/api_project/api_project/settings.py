637ad3d31d25c68c2b9ccdd8c45a15eb14790cb1
Patrick-startech <asamoahpatrick1999@gmail.com>
2025-11-22 00:59:53 -0500

    Add files via upload

--- api_project/api_project/api_project/settings.py
+++ api_project/api_project/api_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api_project',  
     'rest_framework', # Added
     'api', # Added
 ]
