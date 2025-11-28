9bbe1c57bd09c60ece841ab855ae4dd06674bb9b
Patrick-startech <asamoahpatrick1999@gmail.com>
2025-11-28 15:21:10 -0500

    Add files via upload

--- advanced-api-project/advanced_api_project/api/admin.py
+++ advanced-api-project/advanced_api_project/api/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.contrib import admin
 from .models import Author, Book
 # Register your models here.
+
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
