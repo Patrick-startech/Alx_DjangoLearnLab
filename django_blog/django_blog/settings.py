c5e3eb9d0e1bfeededae7817992cee69e90af998
Patrick-startech <asamoahpatrick1999@gmail.com>
2025-12-07 19:37:33 -0500

    Add files via upload

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -15,6 +15,12 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# django_blog/settings.py
+# import os
+SECRET_KEY = 'change-me-in-production'
+DEBUG = True
+ALLOWED_HOSTS = []
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -37,19 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig',
-    'taggit', # My app
+    'blog',
+    'taggit', # My app # <-- add this line
 ]
-
-# django_blog/settings.py
-import os
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-SECRET_KEY = 'change-me-in-production'
-DEBUG = True
-ALLOWED_HOSTS = []
 
 
 MIDDLEWARE = [
